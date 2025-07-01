932e8824864a3ecc7ee5d862951c0006e712f23b
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-30 20:11:25 -0400

    Add files via upload

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+
+// Account, User, Transactions, QuickAction
+
+export interface Account{
+
+}
+
+export interface User{
+
+}
+
+export interface Transactions{
+
+}
+
+export interface QuickAction{
+    
+}
