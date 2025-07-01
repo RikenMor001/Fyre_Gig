27a4135ee845a5cdca885d84d75c661ebd68c7d7
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-30 22:34:45 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import Dashboard from "./components/Dashboard"
+import { mockAccounts, mockQuickActions, mockTransactions, mockUser } from "./data/mockData"
 
-function App() {
-  return <div>
-    Hello 
-  </div>
+function App(){
+  return(
+    <Dashboard
+    user = {mockUser}
+    account = {mockAccounts}
+    transaction = {mockTransactions}
+    quickActions = {mockQuickActions}
+    />
+  )
 }
 
 export default App
