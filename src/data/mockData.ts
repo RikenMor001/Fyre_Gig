6539027357cc1607028ba3576498ce6ae2d1ac08
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-30 21:56:59 -0400

    Add files via upload

---
+++ src/data/mockData.ts
@@ -0,0 +1,9 @@
+import type { User } from "../types";
+
+export const mockUser: User = {
+    id: '1',
+    name: 'Harvey Specter',
+    email: 'harvey.specter@email.com',
+    phone: '+1 (555)-123-4567',
+    memberSince: '2019-03-15'
+}
