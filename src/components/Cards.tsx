f7a7da74b3e939003ccd917aadae525e76116ffd
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-03 02:03:12 -0400

    Add files via upload

---
+++ src/components/Cards.tsx
@@ -0,0 +1,11 @@
+
+
+export function Card(){
+    return <div className="border border-slate-200 bg-white shadow-sm">
+        <div className="flex justify-center items-center">
+            <div className="text-lh tracking-wide font-medium px-5">
+                Card will come here
+            </div>
+        </div>
+    </div>
+}
