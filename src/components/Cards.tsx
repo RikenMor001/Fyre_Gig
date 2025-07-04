5fbe906a8c350d6e1714f2f10d4a730467e95e64
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-04 01:17:23 -0400

    Update Cards.tsx

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,5 +1,6 @@
 import type React from "react";
 import type { Account } from "../types";
+import { CreditCard, PiggyBank, TrendingUp, Wallet } from "lucide-react";
 
 interface AccountProps{
     accounts: Account;
@@ -8,10 +9,24 @@ interface AccountProps{
 
 const Card: React.FC<AccountProps> = ({accounts, onClick}) => {
     console.log(accounts.id)
+
+    const accountIcon = (type: Account['type']) => {
+        switch(type){
+            case "checking":
+                return <Wallet className="h-6 w-6 text-gray-700"/>;
+            case "savings":
+                return <PiggyBank className="h-6 w-6 text-gray-700"/>;
+            case "credit":
+                return <CreditCard className="h-6 w-6 text-gray-700"/>;
+            case "investment":
+                return <TrendingUp className="h-6 w-6 text-gray-700"/>
+        }
+    }
+
     return <div 
     className="bg-white rounded-xl shadow-sm border border-gray-100 p-6 hover:shadow-md transition-all duration-200 cursor-pointer transform hover:-translate-y-1 py-25" 
     onClick={onClick}>
-        Savings
+        {accountIcon(accounts.type)}
     </div>
 }
 
