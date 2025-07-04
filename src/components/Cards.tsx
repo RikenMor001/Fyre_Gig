adbf39e5edf5c35723d629b33d818d16842cb248
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-04 01:04:59 -0400

    Update Cards.tsx

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,18 @@
+import type React from "react";
+import type { Account } from "../types";
 
+interface AccountProps{
+    accounts: Account;
+    onClick: () => void;
+}
 
-export function Card(){
-    return <div className="bg-white rounded-xl shadow-sm border border-gray-100 p-6 hover:shadow-md transition-all duration-200 cursor-pointer transform hover:-translate-y-1 py-25">
+const Card: React.FC<AccountProps> = ({accounts, onClick}) => {
+    console.log(accounts.id)
+    return <div 
+    className="bg-white rounded-xl shadow-sm border border-gray-100 p-6 hover:shadow-md transition-all duration-200 cursor-pointer transform hover:-translate-y-1 py-25" 
+    onClick={onClick}>
         Savings
     </div>
 }
+
+export default Card;
