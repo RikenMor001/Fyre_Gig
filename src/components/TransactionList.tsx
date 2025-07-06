4dc390f04c04045d969c9033bb271427774a4ccf
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-06 00:39:46 -0400

    Update TransactionList.tsx

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -2,6 +2,7 @@
 import type React from "react";
 import type { Transactions } from "../types";
 import { useState } from "react";
+import { CheckCircle, Clock, XCircle } from "lucide-react";
 
 interface TransactionListProps {
     transaction: Transactions[];
@@ -14,6 +15,20 @@ const TransactionLists: React.FC<TransactionListProps> = ({transaction, title = 
 
     console.log({transaction, title})
 
+    const getStatusIcon = (status: Transactions['status']) => {
+    switch (status) {
+      case 'completed':
+        return <CheckCircle className="w-4 h-4 text-green-500" />;
+      case 'pending':
+        return <Clock className="w-4 h-4 text-yellow-500" />;
+      case 'cancelled':
+        return <XCircle className="w-4 h-4 text-red-500" />;
+      default:
+        return null;
+    }
+  };
+
+    
     return <div>
         hello there from the transactionsList
     </div>
